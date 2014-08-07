27c12959c5119831978e2edd7b54c9bbb7328082
yglazko <yglazko@yglazko.local>
2014-08-07 01:08:29 -0700

    Adding some in-progress work

---
+++ js/facts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+window.onload = function() {
+
+	document.getElementById("build-id").innerHTML = navigator.buildID;
+	document.getElementById("gaia").innerHTML = navigator.gaia;
+	document.getElementById("memory").innerHTML = navigator.getFeature("hardware.memory");
+
+	//Going to use the Settings API to get stuff
+	var lock = navigator.mozSettings.createLock();
+
+	//O.S.
+	var os = lock.get('deviceinfo.os');
+	os.onsuccess = function () {
+		var json = JSON.stringify(os.result), obj = JSON.parse(json);
+		document.getElementById("os").innerHTML = obj["deviceinfo.os"];
+	}
+
+	//Platform Version
+	var pltVersion = lock.get('deviceinfo.platform_version');
+	pltVersion.onsuccess = function () {
+		var json = JSON.stringify(pltVersion.result), obj = JSON.parse(json);
+		document.getElementById("platform").innerHTML = obj["deviceinfo.platform_version"];
+	}
+	pltVersion.onerror = function () {
+  console.warn('An error occured: ' + geckoRev.error);
+	}
+
+	//Gecko Version
+	var geckoRev = lock.get('deviceinfo.firmware_revision');
+	geckoRev.onsuccess = function() {
+		var json2 = JSON.stringify(geckoRev.result), obj2 = JSON.parse(json2);
+		console.log(obj2["deviceinfo.firmware_revision"]);
+		document.getElementById("gecko").innerHTML = obj2["deviceinfo.firmware_revision"];
+	}
+
+	geckoRev.onerror = function () {
+  console.warn('An error occured: ' + geckoRev.error);
+	}
+	
+	
+
+};
